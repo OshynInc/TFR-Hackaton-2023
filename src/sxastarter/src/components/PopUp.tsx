7c7416bc78da4005a2a0c6b1b196ea2dd652a76e
Freddy Rueda <frueda@oshyn.com>
2023-03-04 12:01:37 -0500

    Pop Up markup

---
+++ src/sxastarter/src/components/PopUp.tsx
@@ -0,0 +1,18 @@
+import { Text, Field, RichText } from '@sitecore-jss/sitecore-jss-nextjs';
+
+type PopUpProps = {
+  fields: {
+    Header: Field<string>,
+    Content: Field<string>  
+  };
+}
+
+const PopUp = (props: PopUpProps): JSX.Element => (
+	
+  <div>
+    Pop Up
+	<Text field={props.fields.Header} />
+  </div>
+);
+
+export default PopUp;
